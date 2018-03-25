4d08be4da3a7925d1a82ce393a4e36dfb26d20de
Ederlon Barbosa <ederlonbarbosa@gmail.com>
2018-03-25 00:45:08 -0300

    Criação do projeto

---
+++ src/main/java/com/ederlonbarbosa/sistemaDePedidos/SistemaDePedidosApplication.java
@@ -0,0 +1,12 @@
+package com.ederlonbarbosa.sistemaDePedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+}
