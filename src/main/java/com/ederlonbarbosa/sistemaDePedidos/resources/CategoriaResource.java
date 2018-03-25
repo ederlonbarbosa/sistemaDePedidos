c012abace8aa80df9b35518f9165344c512e0c6e
Ederlon Barbosa <ederlonbarbosa@gmail.com>
2018-03-25 01:16:03 -0300

    Criação da classe de dominio Categoria

--- src/main/java/com/ederlonbarbosa/sistemaDePedidos/resources/CategoriaResource.java
+++ src/main/java/com/ederlonbarbosa/sistemaDePedidos/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.ederlonbarbosa.sistemaDePedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ederlonbarbosa.sistemaDePedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String lista() {
-		return "Primeiro teste do REST";
+	public List<Categoria> lista() {
+		Categoria informatica = new Categoria(1, "Informática");
+		Categoria escritorio = new Categoria(2, "Escritório");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(informatica);
+		categorias.add(escritorio);
+
+		return categorias;
 	}
 
 }
