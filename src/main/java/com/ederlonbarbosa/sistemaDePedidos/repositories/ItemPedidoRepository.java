a3cec59dada62b071d34a0199753c1231ed4c46e
Ederlon Barbosa <ederlonbarbosa@gmail.com>
2018-04-08 11:17:45 -0300

    Criacao de ItemPedidoRepository; Add anotation @Repository em EnderecoRepository

--- src/main/java/com/ederlonbarbosa/sistemaDePedidos/repositories/ItemPedidoRepository.java
+++ src/main/java/com/ederlonbarbosa/sistemaDePedidos/repositories/ItemPedidoRepository.java
@@ -1,9 +1,9 @@
 package com.ederlonbarbosa.sistemaDePedidos.repositories;
 
-import com.ederlonbarbosa.sistemaDePedidos.domain.Endereco;
+import com.ederlonbarbosa.sistemaDePedidos.domain.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
 }
